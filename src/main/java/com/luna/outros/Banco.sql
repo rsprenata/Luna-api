12e8c8c9c51399ad8105aa214242deef48f1e6d3
rsprenata <pereira.renatasoares@gmail.com>
2024-07-24 20:29:08 -0300

    aaa

--- src/main/java/com/luna/outros/Banco.sql
+++ src/main/java/com/luna/outros/Banco.sql
@@ -12,3 +12,25 @@ CREATE TABLE artista (
     altura VARCHAR(10) NOT null,
     experiencia TEXT NOT NULL
 );
+
+DROP TABLE vaga;
+CREATE TABLE vaga (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	nome VARCHAR(128),
+    descricao TEXT,
+    valor VARCHAR(10),
+    data VARCHAR(12),
+    qtd_vagas INTEGER
+);
+INSERT INTO vaga (nome, descricao, valor, data, qtd_vagas) VALUES
+('Teste filme', 'blabalbalbabalblalb', 'R$1300,00', '15/08/1990', 3),
+('Ilustrador', 'Responsável por criar ilustrações para livros infantis.', 'R$ 2500', '2024-07-30', 3),
+('Designer Gráfico', 'Criação de artes para mídias sociais e materiais impressos.', 'R$ 3000', '2024-08-01', 2),
+('Fotógrafo', 'Fotografia de eventos e sessões de fotos.', 'R$ 4000', '2024-07-25', 1),
+('Artista Plástico', 'Criação de obras de arte para galerias e exposições.', 'R$ 3500', '2024-08-15', 2),
+('Cenógrafo', 'Desenvolvimento de cenários para peças de teatro.', 'R$ 4500', '2024-09-01', 1),
+('Escultor', 'Criação de esculturas para exposições e vendas.', 'R$ 3800', '2024-07-28', 1),
+('Diretor de Arte', 'Coordenação de projetos de design e publicidade.', 'R$ 5000', '2024-08-10', 1),
+('Animador 2D', 'Criação de animações 2D para vídeos e jogos.', 'R$ 3200', '2024-07-31', 2),
+('Curador de Arte', 'Organização e curadoria de exposições de arte.', 'R$ 4500', '2024-09-05', 1),
+('Produtor Musical', 'Produção e mixagem de trilhas sonoras.', 'R$ 5500', '2024-08-20', 1);
---
+++ src/main/java/com/luna/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package com.luna.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luna.model.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Integer>{
+    
+}
